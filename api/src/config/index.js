b36c5c293a9aebe80155f1d9b2a68d778abc7510
seunggu <imaster0209@naver.com>
2019-06-03 08:06:01 +0900

    config 추가

---
+++ api/src/config/index.js
@@ -0,0 +1,7 @@
+const config = {
+    port: 4000,
+    iamportApiKey: process.env.IAMPORT_API_KEY,
+    iamportApiSecret: process.env.IAMPORT_API_SECRET,
+};
+
+module.exports = config;
--- api/src/index.js
+++ api/src/index.js
@@ -1,3 +1,8 @@
+require('dotenv-safe').config();
+
+const config = require('./config');
+
 const app = require('./server');
-const port = 4000;
+
+const port = config.port;
 app.listen(port, () => console.log(`server start (port: ${port})`));
