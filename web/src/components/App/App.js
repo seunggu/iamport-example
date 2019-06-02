bbfefebefd5f45430795d39fe53a53827369daa1
seunggu <imaster0209@naver.com>
2019-06-03 07:36:00 +0900

    가맹점 식별코드 초기화 삭제
    - 이후 결제 요청시에 초기화 하도록 변경

--- web/src/components/App/App.js
+++ web/src/components/App/App.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import PaymentButton from '../PaymentButton/PaymentButton';
 
 export default class App extends Component {
-  
-  componentDidMount() {
-    this.initIamportId();
-  }
-
-  initIamportId = () => {
-    const IMP = window.IMP;
-    const iamportId = process.env.IAMPORT_ID;
-    IMP.init(iamportId);
-  }
 
   render() {
     return (
       <div className="App">
+        <PaymentButton />
       </div>
     );
   }
--- web/src/index.js
+++ web/src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import dotenv from 'dotenv';
 import './index.css';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
-
-// dot env 
-dotenv.config();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
