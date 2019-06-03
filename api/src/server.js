56595d4aa4ce2210f3dc7cb4f03ab983e531aa76
seunggu <imaster0209@naver.com>
2019-06-03 16:21:12 +0900

    API 서버에 cors 추가

--- api/src/server.js
+++ api/src/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
-const setRouter = require('./routers')
+const setRouter = require('./routers');
 
 const app = express();
 
 // middlewares
+app.use(cors());
 app.use(express.json())
 
 // routers
